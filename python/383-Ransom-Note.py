7c7faf4ff35a36572ba79051476aebcb719978ed
jjsizemore <jermainesizemore@gmail.com>
2023-01-21 13:59:30 -0800

    Add Ransom Note soln py

---
+++ python/383-Ransom-Note.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+class Solution:
+
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        magCount = defaultdict(lambda: 0)
+
+        for char in magazine:
+            magCount[char] += 1
+
+        for char in ransomNote:
+            magCount[char] -= 1
+            if magCount[char] < 0:
+                return False
+        return True
