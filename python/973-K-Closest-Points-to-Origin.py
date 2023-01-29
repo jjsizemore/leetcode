0091579df7e72f1d2f5304565c38470837f48d63
jjsizemore <jermainesizemore@gmail.com>
2023-01-29 12:44:07 -0800

    Add K closest pts to origin py soln

---
+++ python/973-K-Closest-Points-to-Origin.py
@@ -0,0 +1,9 @@
+# region Sort
+# Time O(nlogn)
+# Space O(n)
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        points.sort(key=lambda point: point[0]**2 + point[1]**2)
+        return points[:k]
+
+# endregion
