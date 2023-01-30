93f377efaef45ed56c43e78176a73808da51468c
jjsizemore <jermainesizemore@gmail.com>
2023-01-30 07:03:11 -0800

    Add heapsort for k closest pts python

--- python/973-K-Closest-Points-to-Origin.py
+++ python/973-K-Closest-Points-to-Origin.py
@@ -1,3 +1,26 @@
+# region Min Heap
+# Time O(nlogk) -- Inserting into heap of size k takes O(logk) time & we do that n times
+# Space O(k)
+import heapq
+
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap = []
+
+        # Push the points into a minheap, where we negate the distance
+        # Negating distance means that when we pop, we pop the largest absolute distance
+        for x, y in points:
+            dist = -(x**2 + y**2)
+            if len(heap) == k:
+                heapq.heappushpop(heap, (dist, x, y))
+            else:
+                heapq.heappush(heap, (dist, x, y))
+
+        return [(x, y) for (dist, x, y) in heap]
+# endregion
+
+
 # region Sort
 # Time O(nlogn)
 # Space O(n)
