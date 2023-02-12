47a4f23d078b15d8672db1e2d27625b47d2e310d
jjsizemore <jermainesizemore@gmail.com>
2023-02-11 16:29:38 -0800

    Add time based KV store py soln

---
+++ python/981-Time-Based-Key-Value-Store.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+# region Dict and Reverse Linear Search
+# Time O
+class TimeMap:
+    def __init__(self):
+        self.timeMap = defaultdict(lambda: {})
+
+    # Time O(1)
+    # Space O(1)
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.timeMap[key][timestamp] = value
+
+    # Time O(N) where N is the number of timestamped values for key
+    # Space O(N) where N is the number of timestamped values for key, for the reversed copy of timestamp keys
+    def get(self, key: str, timestamp: int) -> str:
+        for time in reversed(self.timeMap[key]):
+            if self.timeMap[key][time] and time <= timestamp:
+                return self.timeMap[key][time]
+        return ""
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
+# endregion
