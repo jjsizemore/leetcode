d2da6badc06aa2f7700b48fd9dae8de26e3fdcd3
jjsizemore <jermainesizemore@gmail.com>
2023-02-21 08:20:42 -0800

    Add Disjoint Set Union soln for Accounts Merge

--- python/721-Accounts-Merge.py
+++ python/721-Accounts-Merge.py
@@ -1,7 +1,52 @@
-# region DFS
-
 from collections import defaultdict
+import collections
 from typing import List
+
+# region Disjoint Set Union (DSU)
+
+# Sets up accounts as groups of nodes represented by indices in the parents array
+# Manages relationships between the groups by manipulating the parent values
+class DSU:
+    def __init__(self, size):
+        self.parents = list(range(size))
+
+    # Recursively find the parent node of the set
+    def findRoot(self, x: int) -> int:
+        if x != self.parents[x]:
+            self.parents[x] = self.findRoot(self.parents[x])
+        return self.parents[x]
+
+    # Set the parent of the root of the child equal to the root of the parent
+    def union(self, child: int, parent: int) -> None:
+        self.parents[self.findRoot(child)] = self.findRoot(parent)
+
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        # Holds acct to parent group mapping
+        accountMap = dict()
+
+        dsu = DSU(len(accounts))
+
+        for acctIdx, account in enumerate(accounts):
+            for email in account[1:]:
+                # If we've already seen the email, set cur acct group to have the same parent as the one the seen email is already associated with
+                if email in accountMap:
+                    dsu.union(acctIdx, dsu.findRoot(accountMap[email]))
+                # We always set each email to map to its group -- this will be used to recursively find the email root group
+                accountMap[email] = acctIdx
+
+        retVal = collections.defaultdict(list)
+
+        for email, parent in accountMap.items():
+            retVal[dsu.findRoot(parent)].append(email)
+
+        return [[accounts[i][0]] + sorted(emails) for i, emails in retVal.items()]
+
+
+# endregion
+
+# region DFS
 
 
 class Solution:
