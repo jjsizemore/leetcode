02470c8198ae4fcee60a4303dcb326ea75c8924d
jjsizemore <jermainesizemore@gmail.com>
2023-03-14 08:37:46 -0700

    Add Kth Smallest Elem in BST

---
+++ python/230-Kth-Smallest-Element-in-a-BST.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# region Smallest Elem Stack
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+
+        def _scan_left(smallest, stack):
+            stack.append(smallest)
+            while smallest.left:
+                smallest = smallest.left
+                stack.append(smallest)
+
+        _scan_left(root, stack)
+
+        while k > 1:
+            cur = stack.pop()
+            k -= 1
+            if cur.right:
+                _scan_left(cur.right, stack)
+
+        return stack[-1].val
+
+
+# endregion
