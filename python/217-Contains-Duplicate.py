1fb3d85d0bd6ad606021a8c114a5d536dcf47ecb
jjsizemore <jermainesizemore@gmail.com>
2023-03-14 08:18:55 -0700

    Add Contains Duplicate

---
+++ python/217-Contains-Duplicate.py
@@ -0,0 +1,15 @@
+# region Tracker Set
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        s = set()
+        for num in nums:
+            if num in s:
+                return True
+            s.add(num)
+        return False
+
+
+# endregion
