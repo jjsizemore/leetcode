f7286e80e85fc727130e1ef830db5d232ede9006
jjsizemore <jermainesizemore@gmail.com>
2023-03-15 08:24:40 -0700

    Add iterative inorder traversal for kth smallest elem in BST

--- python/230-Kth-Smallest-Element-in-a-BST.py
+++ python/230-Kth-Smallest-Element-in-a-BST.py
@@ -9,6 +9,26 @@ class TreeNode:
         self.right = right
 
 
+# region Simple Iterative In Order Traversal
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+
+        while True:
+            while root:
+                stack.append(root)
+                root = root.left
+
+            root = stack.pop()
+
+            k -= 1
+            if not k:
+                return root.val
+            root = root.right
+
+
+# endregion
+
 # region Smallest Elem Stack
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
