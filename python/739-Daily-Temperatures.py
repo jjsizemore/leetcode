f0e265da3951a9b119c41e7863f3d730431d4e0f
jjsizemore <jermainesizemore@gmail.com>
2023-09-24 17:17:38 -0700

    Add monotonic stack daily temps soln

--- python/739-Daily-Temperatures.py
+++ python/739-Daily-Temperatures.py
@@ -21,3 +21,26 @@ class Solution:
 
 
 # endregion
+
+# region Monotonic Stack
+# Monotonic Stack - A stack where the elements are always in sorted order
+# Monotonic decreasing, like the one below, means the stack will always be sorted in descending order
+# We store the indices, but the indices are sorted in descending order in terms of the temperatures they correspond to
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []
+
+        for cur_day, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                prev_day = stack.pop()
+                answer[prev_day] = cur_day - prev_day
+            stack.append(cur_day)
+
+        return answer
+
+
+# endregion
