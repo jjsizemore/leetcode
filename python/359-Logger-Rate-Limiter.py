cd9c01c714ad125d7457f546a7de4bfcfec8ae18
jjsizemore <jermainesizemore@gmail.com>
2023-10-12 10:10:24 -0700

    Add logger rate limiter queue & set soln

--- python/359-Logger-Rate-Limiter.py
+++ python/359-Logger-Rate-Limiter.py
@@ -1,5 +1,38 @@
 import collections
 
+# region Set & Queue
+
+
+class Logger:
+    def __init__(self):
+        self.q = collections.deque()
+        self.msgs = set()
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        while self.q:
+            ts, msg = self.q[0]
+            if ts > timestamp - 10:
+                break
+            else:
+                self.q.popleft()
+                self.msgs.remove(msg)
+
+        if message in self.msgs:
+            return False
+        else:
+            self.q.append((timestamp, message))
+            self.msgs.add(message)
+            return True
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
+
+# endregion Set & Queue
+
+# region HashMap
+
 
 class Logger:
     def __init__(self):
@@ -12,6 +45,8 @@ class Logger:
         self.msgs[timestamp].add(message)
         return True
 
+
+# endregion HashMap
 
 # Your Logger object will be instantiated and called as such:
 # obj = Logger()
