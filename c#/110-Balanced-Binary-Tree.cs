d0568cce8030510ba5d75997dd64d3c4d8fdd3fa
Jermaine Sizemore <jermainesizemore@gmail.com>
2025-06-03 23:42:13 -0700

    Add DFS soln for LC110 c#

--- c#/110-Balanced-Binary-Tree.cs
+++ c#/110-Balanced-Binary-Tree.cs
@@ -1,3 +1,35 @@
+#region DFS
+// Time O(n) - n is the number of nodes in the tree
+// Space O(h) - h is the height of the tree
+//  Best Case (Balanced Tree): O(log(n))
+//  Worst Case (Skewed Tree): O(n)
+public class Solution {
+    public bool IsBalanced(TreeNode root) {
+        return GetDepth(root) != -1;
+    }
+
+    public int GetDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftD = GetDepth(root.left);
+        if (leftD == -1) {
+            return -1;
+        }
+
+        int rightD = GetDepth(root.right);
+        if (rightD == -1) {
+            return -1;
+        }
+
+        if (Math.Abs(leftD - rightD) > 1) {
+            return -1;
+        }
+
+        return Math.Max(leftD, rightD) + 1;
+    }
+}
+#endregion
+
 // Time		O(n*log(n))
 // Space	O(N)
 public class Solution
