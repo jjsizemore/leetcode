75e8fb9798b8a361902fef6db83ccc7b0a8f14e2
Jermaine Sizemore <jermainesizemore@gmail.com>
2025-06-05 09:51:25 -0700

    Add solution for "Count Good Nodes in Binary Tree" with detailed XML documentation and DFS approach

--- c#/1448-Count-Good-Nodes-in-Binary-Tree.cs
+++ c#/1448-Count-Good-Nodes-in-Binary-Tree.cs
@@ -1,17 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
+#region
+/// <summary>
+/// 1448. Count Good Nodes in Binary Tree
+/// https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+///
+/// Given a binary tree root, a node X in the tree is named good if in the path from root to X there are no nodes with a value greater than X.
+/// Return the number of good nodes in the binary tree.
+///
+/// Example 1:
+/// Input: root = [3,1,4,3,null,1,5]
+/// Output: 4
+///
+/// Example 2:
+/// Input: root = [3,3,null,4,2]
+/// Output: 3
+///
+/// Example 3:
+/// Input: root = [1]
+/// Output: 1
+///
+/// DFS with a helper function to keep track of the highest value seen so far.
+/// If the current node's value is greater than or equal to the highest value seen so far,
+/// then it is a good node.
+/// We then update the highest value seen so far to the current node's value and continue
+/// the DFS.
+/// </summary>
 public class Solution {
     public int count = 0;
 
@@ -36,3 +48,4 @@ public class Solution {
         return count;
     }
 }
+#endregion
